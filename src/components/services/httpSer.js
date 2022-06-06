f957a3d5fd7b2e4dbe65e7e5f9fc2dd27b9e991e
Sunil Dhakad <dhakadsunil158@gmail.com>
2022-06-06 19:30:23 +0530

    server update

--- src/components/services/httpSer.js
+++ src/components/services/httpSer.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-//const baseURL="http://localhost:2410";
-const baseURL="https://nameless-ocean-23217.herokuapp.com";
+const baseURL="http://localhost:2410";
 function get(url){
     return axios.get(baseURL + url);
 }
